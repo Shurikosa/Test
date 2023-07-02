d4ea47388c0a665f86431d2507d32a6ffa67403d
Albina <yang679.lit@gmail.com>
2023-07-02 21:58:56 +0300

    FileCity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    List <String> namesCity = new ArrayList<String>();
     public static void main(String[] args) {
         System.out.println("Hello!");
-    }
 
-}
-class Dimkageo {
+    }
+    public void getNameFile() {
+        try {
+            namesCity = Files.readAllLines(Paths.get("city.txt"), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
